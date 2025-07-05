9f17ceb5081a6da29d3ec0ffa17cb06570e25e10
JacobN-dev <kuba123narloch@wp.pl>
2025-07-05 17:57:44 +0200

    Update Window.h

--- Window.h
+++ Window.h
@@ -7,19 +7,19 @@ private:
 	class WindowClass
 	{
 	public:
-		static const char* GetName() noexcept;
+		static LPCWSTR GetName() noexcept;
 		static HINSTANCE GetInstance() noexcept;
 	private:
 		WindowClass() noexcept;
 		~WindowClass();
 		WindowClass(const WindowClass&) = delete;
 		WindowClass& operator=(const WindowClass&) = delete;
-		static constexpr const char* wndClassName = "Chili Direct3D Engine Window";
+		static constexpr LPCWSTR wndClassName = "Chili Direct3D Engine Window";
 		static WindowClass wndClass;
 		HINSTANCE hInst;
 	};
 public:
-	Window(int width, int height, const char* name) noexcept; ~Window();
+	Window(int width, int height, LPCWSTR name) noexcept; ~Window();
 	Window (const Window&) = delete;
 	Window& operator=(const Window&) = delete;
 private:
